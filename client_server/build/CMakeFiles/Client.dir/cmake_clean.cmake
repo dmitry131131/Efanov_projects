c1746c3552aa21886f79cff3c76fc29e911cc772
Dmitry Antontsev <antontsev.dmitry@yandex.ru>
2024-12-06 00:04:59 +0300

    [Client_server] made base structure

---
+++ client_server/build/CMakeFiles/Client.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Client.dir/link.d"
+  "CMakeFiles/Client.dir/client/src/main.c.o"
+  "CMakeFiles/Client.dir/client/src/main.c.o.d"
+  "bin/Client"
+  "bin/Client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ client_server/build/CMakeFiles/Server.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Server.dir/link.d"
+  "CMakeFiles/Server.dir/server/src/main.c.o"
+  "CMakeFiles/Server.dir/server/src/main.c.o.d"
+  "bin/Server"
+  "bin/Server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
